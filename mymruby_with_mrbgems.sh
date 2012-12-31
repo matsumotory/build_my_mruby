248843e4999442f86fb3675044c4f762fa662d96
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2012-12-31 16:41:28 +0900

    Add mruby-digest, mruby-pack, mruby-oauth

--- mymruby_with_mrbgems.sh
+++ mymruby_with_mrbgems.sh
@@ -7,16 +7,16 @@ git clone git://github.com/mattn/mruby-uv.git
 git clone git://github.com/mattn/mruby-http.git
 git clone git://github.com/mattn/mruby-json.git
 git clone git://github.com/mattn/mruby-curl.git
-git clone git://github.com/mattn/mruby-md5.git
-git clone git://github.com/mattn/mruby-sha1.git
-git clone git://github.com/mattn/mruby-base64.git
 git clone git://github.com/matsumoto-r/mruby-redis.git
+git clone git://github.com/matsumoto-r/mruby-random.git
+git clone git://github.com/iij/mruby-digest.git
+git clone git://github.com/iij/mruby-pack.git
+git clone git://github.com/iij/mruby-mtest.git
 git clone git://github.com/matsumoto-r/mruby-simplehttp.git
 git clone git://github.com/matsumoto-r/mruby-httprequest.git
+git clone git://github.com/matsumoto-r/mruby-oauth.git
 git clone git://github.com/matsumoto-r/mruby-growthforecast.git
 git clone git://github.com/matsumoto-r/mruby-zabbix.git
-git clone git://github.com/matsumoto-r/mruby-random.git
-git clone git://github.com/iij/mruby-mtest.git
 cd -
 
 cd mruby
@@ -26,15 +26,15 @@ mruby-uv
 mruby-http
 mruby-redis
 mruby-curl
-mruby-md5
-mruby-sha1
-mruby-base64
+mruby-digest
+mruby-pack
+mruby-mtest
 mruby-simplehttp
 mruby-httprequest
+mruby-oauth
 mruby-growthforecast
 mruby-zabbix
 mruby-random
-mruby-mtest
 EOF
 
 rake ENABLE_GEMS='true'
